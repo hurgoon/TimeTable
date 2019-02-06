8b9341d5f7a8a0d9a0b74f956bc00d612de79bb5
Bernard <hurgoon@gmail.com>
2019-02-06 16:27:33 +0900

    [JS] Project created

---
+++ TimeTableProject/TimeTableProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTableProject
+//
+//  Created by Bernard Hur on 06/02/2019.
+//  Copyright © 2019 Bernard Hur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
